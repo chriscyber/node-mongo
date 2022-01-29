97911d172853d0af08acfc0db95ce439073bb104
Christine Anthony <2christineanthony@gmail.com>
2022-01-28 20:01:34 -0500

    Node Callback Hell and Promises - refactored from nested callbacks to promises

--- index.js
+++ index.js
@@ -1,57 +1,63 @@
 const MongoClient = require("mongodb").MongoClient; //driver that creates interface entre db and application
-const assert = require("assert").strict;
 const dboper = require("./operations");
 
 const url = "mongodb://localhost:27017/"; //connection to mongo server. port on which server is running
 const dbname = "nucampsite"; //db to connect to
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  assert.strictEqual(err, null); //actual, expected. if they're not ===, assert fails, logs error, and terminates
-  console.log("Connected correctly to mongodb server");
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected correctly to mongodb server");
 
-  const db = client.db(dbname);
+    const db = client.db(dbname);
 
-  //dbname.campsite.insertOne({ name: "temp" });
+    db.dropCollection("campsites")
+      .then((result) => {
+        console.log("Dropped Collection", result);
+      })
+      .catch((err) => console.log("No collection to drop."));
 
-  db.dropCollection("campsites", (err, result) => {
-    assert.strictEqual(err, null);
-    console.log("Dropped Collection", result);
+    dboper
+      .insertDocument(
+        db,
+        { name: "Breadcrumb Trail Campground", description: "Test" },
+        "campsites"
+      )
+      .then((result) => {
+        console.log("Insert Document:", result.ops);
 
-    dboper.insertDocument(
-      db,
-      { name: "Breadcrumb Trail Campground", description: "Test" },
-      "campsites",
-      (result) => {
-        console.log("Insert Document:", result.ops); //this inline callback definiton will remember callback result later.
+        return dboper.findDocuments(db, "campsites");
+      })
+      .then((docs) => {
+        console.log("Found Documents:", docs);
 
-        dboper.findDocuments(db, "campsites", (docs) => {
-          console.log("Found Documents:", docs);
+        return dboper.updateDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          { description: "Updated Test Description" },
+          "campsites"
+        );
+      })
+      .then((result) => {
+        console.log("Updated Doc Count:", result.result.nModified);
 
-          dboper.updateDocument(
-            db,
-            { name: "Breadcrumb Trail Campground" },
-            { description: "Updated Test Description" },
-            "campsites",
-            (result) => {
-              console.log("Updated Doc Count:", result.result.nModified);
+        return dboper.findDocuments(db, "campsites");
+      })
+      .then((docs) => {
+        console.log("Found Docs:", docs);
 
-              dboper.findDocuments(db, "campsites", (docs) => {
-                console.log("Found Docs:", docs);
-
-                dboper.removeDocument(
-                  db,
-                  { name: "Breadcrumb Trail Campground" },
-                  "campsites",
-                  (result) => {
-                    console.log("Deleted Doc Count:", result.deletedCount);
-                    client.close(); //closes connection from server
-                  }
-                );
-              });
-            }
-          );
-        });
-      }
-    );
-  });
-});
+        return dboper.removeDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          "campsites"
+        );
+      })
+      .then((result) => {
+        console.log("Deleted Doc Count:", result.deletedCount);
+        return client.close(); //closes connection from server
+      })
+      .catch((err) => {
+        console.log(err);
+        client.close();
+      });
+  })
+  .catch((err) => console.log(err));
--- operations.js
+++ operations.js
@@ -1,35 +1,21 @@
 // CRUD ops
 
-const assert = require("assert").strict;
-
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
   const coll = db.collection(collection); // get access to a particular collection(string)
-  coll.insertOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.insertOne(document); // return returns value as the default promise value
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
   const coll = db.collection(collection);
-  coll.find().toArray((err, docs) => {
-    assert.strictEqual(err, null);
-    callback(docs);
-  });
+  return coll.find().toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.deleteOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
   const coll = db.collection(collection);
-  coll.updateOne(document, { $set: update }, null, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.updateOne(document, { $set: update }, null);
 };
